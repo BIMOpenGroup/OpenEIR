af7cab5affd42be081c5bd9e402aa205fd37fc89
AlexBabinoff <alex.babinoff.cheat@gmail.com>
2021-03-12 19:17:06 +0300

    docs mode spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,63 +1,59 @@
 export default {
-//   mode: 'spa',
+  mode: "spa",
   /*
-  ** Headers of the page
-  */
- generate: {
-	dir: 'docs'
+   ** Headers of the page
+   */
+  generate: {
+    dir: "docs",
   },
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-	'bootstrap-vue/nuxt',
+    "bootstrap-vue/nuxt",
   ],
   bootstrapVue: {
-	bootstrapCSS: false, 
-	bootstrapVueCSS: false
+    bootstrapCSS: true,
+    bootstrapVueCSS: true,
   },
-  css: [
-	  '@/assets/scss/custom.scss'
-	],
+  css: ["@/assets/scss/custom.scss"],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
-  }
-}
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+};
